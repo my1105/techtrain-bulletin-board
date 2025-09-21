2d20825d6c2af40b293cae9ef14dc595e26afd20
Mayu Adachi <ykmshiss@gmail.com>
2025-09-21 22:10:07 +0900

    詳細画面と投稿フォーム

--- src/main.jsx
+++ src/main.jsx
@@ -1,15 +1,17 @@
 import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
 import NewThread from "./NewThread";
+import ThreadDetail from "./ThreadDetail";
 import { createRoot } from "react-dom/client"; 
 import App from "./App.jsx"; 
 
 
 createRoot(document.getElementById("root")).render(
   <BrowserRouter>
-    <Routes>
+      <Routes>
       <Route path="/" element={<Navigate to="/threads" />} />
       <Route path="/threads" element={<App />} />
       <Route path="/threads/new" element={<NewThread />} />
-    </Routes>
+      <Route path="/threads/:thread_id" element={<ThreadDetail />} />
+      </Routes>
   </BrowserRouter>
-);
+  );
